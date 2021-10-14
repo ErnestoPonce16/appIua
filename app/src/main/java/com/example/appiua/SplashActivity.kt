574b6382777dbdadd4a98855201234150253ba71
Ernesto <rponce831@alumnos.iua.edu.ar>
2021-10-14 20:26:25 -0300

    Inicio Proyecto

---
+++ app/src/main/java/com/example/appiua/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.example.appiua
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.asset_fade_in)
+        iv_splash_logo.startAnimation(splashAnimation)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                startActivity(
+                    Intent(this@SplashActivity, MainActivity::class.java)
+                )
+                finish()
+            }, 2000
+
+        )
+    }
+
+
+}
